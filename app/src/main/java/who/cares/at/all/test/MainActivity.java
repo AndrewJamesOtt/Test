12c296b6d5e3e155db36334f62993284c7bfc32a
Andrew Ott <andrewjott@icloud.com>
2016-08-25 17:30:41 -0700

    Initial commit

---
+++ app/src/main/java/who/cares/at/all/test/MainActivity.java
@@ -0,0 +1,16 @@
+package who.cares.at.all.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int fucksGiven = 0;
+        boolean doICare = false;
+    }
+}
